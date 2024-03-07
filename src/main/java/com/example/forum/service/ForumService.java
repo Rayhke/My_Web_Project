3ba84c2cec1398cbb87868b4f83fa5ce32a3f20a
Rayhke <nink2458@gmail.com>
2024-03-07 18:34:54 +0900

    2024-03-07 (PM 6:34)

--- src/main/java/com/example/forum/service/ForumService.java
+++ src/main/java/com/example/forum/service/ForumService.java
@@ -2,23 +2,14 @@ package com.example.forum.service;
 
 import com.example.forum.controller.ForumDTO;
 import com.example.forum.entity.Forum;
-import com.example.forum.repository.ForumRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 public interface ForumService {
-    /*private final ForumRepository forumRepository;
-
-    @Autowired
-    public ForumService(ForumRepository forumRepository) {
-        this.forumRepository = forumRepository;
-    }
-
-    public ForumDTO save(Long id, String author) {
-        return null;
-    }*/
 
     ForumDTO save(Long id, String author);
 
     ForumDTO update(Long id, String author);
+
+    Optional<Forum> findId(Long id);
 }

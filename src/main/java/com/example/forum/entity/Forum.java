d06679cba5edb22561eb9d8f0df09b879e4354ac
Rayhke <nink2458@gmail.com>
2024-03-15 18:28:07 +0900

    24-03-15 (PM 6:27)

--- src/main/java/com/example/forum/entity/Forum.java
+++ src/main/java/com/example/forum/entity/Forum.java
@@ -1,7 +1,5 @@
 package com.example.forum.entity;
 
-
-import com.example.forum.controller.ForumDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
--- src/main/java/com/example/forum/repository/ForumJpaRepository.java
+++ src/main/java/com/example/forum/repository/ForumJpaRepository.java
@@ -1,14 +1,12 @@
 package com.example.forum.repository;
 
 import com.example.forum.entity.Forum;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.sql.DataSource;
 import java.util.List;
 
 @Repository

6230951e998da5ad18aa00c08581709f2cba8af0
Rayhke <nink2458@gmail.com>
2024-03-22 17:55:58 +0900

    2024-03-22 (PM 5:55)

--- src/main/java/com/example/MySpringApplication.java
+++ src/main/java/com/example/MySpringApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing  // 시간 자동 활성화
 @SpringBootApplication
-public class RayhkeSpringApplication {
+public class MySpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RayhkeSpringApplication.class, args);
+		SpringApplication.run(MySpringApplication.class, args);
 	}
 	/*
 		[실행 주소]
